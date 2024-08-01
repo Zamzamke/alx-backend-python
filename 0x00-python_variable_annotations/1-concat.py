f4c8aa1dc939fb0ef7a095ed2083a5b65548c790
Zamzamke <zamzamchelimo@gmail.com>
2024-08-01 21:23:28 +0000

    added files

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function add that takes a strings str1 and str2
+as arguments and returns their combination.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    accepts two string argumnets and concatinate them
+    returning a string
+    """
+    return str1 + str2
